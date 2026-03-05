ef31f128c35bba7b2b9a80075af0823806afbbbd
VIKAS S S <vikas.ss2023ece@sece.ac.in>
2026-03-05 19:47:51 +0530

    created the frontend folder structure

---
+++ wellwater/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    
+      <p className="bg-red-500">
+        Click on the Vite and React logos to learn more
+      </p>
+    
+  )
+}
+
+export default App
